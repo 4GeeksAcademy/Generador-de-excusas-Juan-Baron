ae9f04ae11bab3c7a1de5064ebccd55727d36179
Juan Diego Baron Faura <JDBF1011@Gmail.com>
2025-03-05 11:54:17 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+ 
+  
+  let who = ['mi pajaro','el alien','el agujero negro','mi dinosaurio'];
+let what = ['destruyo','se llevo','robo','se comio'];
+let when = ['antes de llegar a la luna','cuando estaba estacionando','cuando estaba comiendo','mientras dormia','cuando estaba en la cocina'];
+
+let whoRandom = Math.floor(Math.random() * who.length)
+let whatRandom = Math.floor(Math.random() * what.length)
+let whenRandom = Math.floor(Math.random() * when.length)
+
+document.querySelector("#excuse").innerHTML = who[whoRandom] + " " + what[whatRandom] +  " " + when[whenRandom]
   console.log("Hello Rigo from the console!");
 };
